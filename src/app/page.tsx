7cb94935115e851acc1ab5d555746ec32b637a4a
Rishabh Kumar <rishabhgzp2004@gmail.com>
2026-01-29 13:32:17 +0530

    next-auth implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { signIn } from "./actions/sign-in";
-import { signOut } from "./actions/sign-out";
 
-export default function Home() {
+export default async function Home() {
+
   return (
    <div>
     <h1>Home Page</h1>
-    <form action={signIn}>
-      <Button type="submit">signIn</Button>
-    </form>
-    <form action={signOut}>
-      <Button type="submit">signOut</Button>
-    </form>
-    
     </div>
   );
 }
