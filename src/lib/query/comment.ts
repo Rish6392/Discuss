e8648672dd6db5279d5e344b88dae7cebd282d88
Rishabh Kumar <rishabhgzp2004@gmail.com>
2026-02-01 20:25:29 +0530

    nested comment

--- src/lib/query/comment.ts
+++ src/lib/query/comment.ts
@@ -1,4 +1,22 @@
+import { Prisma } from "@prisma/client"
+import type { Comment } from "@prisma/client";
+import { prisma } from "..";
 
-export const fetchCommentByPostId = async (postId:string) => {
-    
+export type CommentWithAuther = Comment & {
+    user:{name:string|null;image:string|null}
+}
+
+
+export const fetchCommentByPostId = async (postId:string):Promise<CommentWithAuther[]> => {
+    return prisma.comment.findMany({
+        where:{postId},
+        include:{
+            user:{
+                select:{
+                    name:true,
+                    image:true
+                }
+            }
+        }
+    })
 }
